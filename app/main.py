ee9acd72f5f899f7582bcca3cdbe025bc98acb44
unknown <hussam123-lab>
2025-09-01 01:18:40 +1000

    added number of tables logic

--- app/main.py
+++ app/main.py
@@ -13,8 +13,12 @@ if command == ".dbinfo":
         print("Logs from your program will appear here!", file=sys.stderr)
 
         # Uncomment this to pass the first stage
-        database_file.seek(16)  # Skip the first 16 bytes of the header
+        database_file.seek(16)
         page_size = int.from_bytes(database_file.read(2), byteorder="big")
         print(f"database page size: {page_size}")
+
+        database_file.seek(28)
+        numbero_of_tables = int.from_bytes(database_file.read(4), byteorder="big")
+        print(f"number of tables: {numbero_of_tables}")
 else:
     print(f"Invalid command: {command}")
